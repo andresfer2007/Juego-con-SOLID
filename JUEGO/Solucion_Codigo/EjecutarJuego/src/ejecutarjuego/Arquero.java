604d0e4a76c0922985ec82d88f92e06ec5f3e6e1
andresfer2007 <andresalejandrofernandeztoledo@gmail.com>
2026-06-28 17:45:44 -0500

    Estado local del proyecto con project.properties corregido

---
+++ JUEGO/Solucion_Codigo/EjecutarJuego/src/ejecutarjuego/Arquero.java
@@ -0,0 +1,45 @@
+package ejecutarjuego;
+
+public class Arquero extends Personaje {
+
+    private int precision;
+
+    public Arquero(String nombre, int vida, int nivel, int precision) {
+        super(nombre, vida, nivel);
+        this.precision = precision;
+    }
+    public int getPrecision(){
+        return precision;
+    }
+
+    @Override
+    public int atacar() {
+        return (precision * nivel) + getBonusAtaque();
+    }
+
+    @Override
+    public int defender() {
+        return 4 + getBonusDefensa();
+    }
+
+    @Override
+    public String toString() {
+        return "\n--- ARQUERO ---"
+                + "\n" + super.toString()
+                + "\nPrecision: " + precision;
+    }
+
+
+
+    //cambios
+    @Override
+    public int usarHabilidadEspecial(){
+        if(energia>=20 && cooldown==0){
+            energia-=20;
+            cooldown=2;
+            return (getPrecision()*nivel)+30;
+        }
+        return atacar();
+    }
+}
+
---
+++ JUEGO/Solucion_Codigo/EjecutarJuego/src/ejecutarjuego/Guerrero.java
@@ -0,0 +1,39 @@
+
+package ejecutarjuego;
+
+public class Guerrero extends Personaje {
+
+    private int fuerza;
+
+    public Guerrero(String nombre, int vida, int nivel, int fuerza) {
+        super(nombre, vida, nivel);
+        this.fuerza = fuerza;
+    }
+
+    @Override
+    public String toString() {
+        return "\n--- GUERRERO ---"
+                + "\n" + super.toString()
+                + "\nFuerza: " + fuerza;
+    }
+
+    @Override
+    public int atacar() {
+        return (fuerza * nivel) + getBonusAtaque(); // incluye bonus del arma
+    }
+
+    @Override
+    public int defender() {
+        return 5 + getBonusDefensa(); // incluye bonus de armadura
+    }
+
+    @Override
+    public int usarHabilidadEspecial(){
+        if(energia>=30 && cooldown==0){
+            energia-=30;
+            cooldown=3;
+            return atacar()*3;
+        }
+        return atacar();
+    }
+}
