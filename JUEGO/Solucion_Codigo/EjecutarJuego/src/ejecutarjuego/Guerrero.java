47e35aa102910bf53c2b738ef1d7fc845f5cb6ad
Mxteo2008 <mateo.jimenez.bvm@gmail.com>
2026-06-29 07:31:17 -0500

    primer

--- JUEGO/Solucion_Codigo/EjecutarJuego/src/ejecutarjuego/Guerrero.java
+++ JUEGO/Solucion_Codigo/EjecutarJuego/src/ejecutarjuego/Guerrero.java
@@ -2,6 +2,10 @@ package ejecutarjuego;
 
 public class Guerrero extends Personaje {
 
+
+    private static final int COSTO_ENERGIA_HABILIDAD = 30;
+    private static final int COOLDOWN_HABILIDAD = 3;
+    private static final int MULTIPLICADOR_HABILIDAD = 3;
     private int fuerza;
 
     public Guerrero(String nombre, int vida, int nivel, int fuerza) {
@@ -11,18 +15,31 @@ public class Guerrero extends Personaje {
 
     @Override
     public int atacar() {
-        return fuerza * nivel;
+        return (fuerza * nivel) + getBonusAtaque();
     }
 
     @Override
     public int defender() {
-        return 5;
+        return 5 + getBonusDefensa();
+    } // LSP: implementa el contrato de Personaje 
+
+    @Override
+    public boolean puedeUsarHabilidad() {
+        return energia >= COSTO_ENERGIA_HABILIDAD && cooldown == 0;
+    } 
+
+    @Override
+    public int usarHabilidadEspecial() {
+        if (puedeUsarHabilidad()) {
+            energia -= COSTO_ENERGIA_HABILIDAD;
+            cooldown = COOLDOWN_HABILIDAD;
+            return atacar() * MULTIPLICADOR_HABILIDAD;
+        }
+        return atacar();
     }
 
     @Override
     public String toString() {
-        return "\n--- GUERRERO ---"
-                + "\n" + super.toString()
-                + "\nFuerza: " + fuerza;
+        return "\n--- GUERRERO ---" + "\n" + super.toString() + "\nFuerza: " + fuerza;
     }
 }
